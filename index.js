268d2f2805200dc31991924d75ab53f2f0b324a4
cwarmbrunn <cwarmbrunn97@gmail.com>
2022-03-30 09:40:35 -0400

    added in README markdown template in generateMarkdown.js

--- index.js
+++ index.js
@@ -7,14 +7,47 @@ const generatePage = require("./src/readme-template");
 
 // Grants access to the file system functionality
 const fs = require("fs");
+
+const { writeFile } = require("./generateMarkdown");
 // TODO: Create an array of questions for user input
-inquirer
-  .prompt([
-    // Question #1 - Project Title
+const promptUser = () => {
+  return inquirer.prompt([
+    {
+      // Question #1 - GitHub Username
+      type: "input",
+      name: "github",
+      message: "What is your GitHub username? (Required)",
+      // Validation if nameInput is blank
+      validate: (nameInput) => {
+        if (nameInput) {
+          return true;
+        } else {
+          console.log("Please enter your GitHub username!");
+          return false;
+        }
+      },
+    },
+    {
+      // Question #2 - What is your email address?
+      type: "input",
+      name: "title",
+      message: "What is your email address? (Required)",
+      // Validation if nameInput is blank
+      validate: (nameInput) => {
+        if (nameInput) {
+          return true;
+        } else {
+          console.log("Please enter an email address!");
+          return false;
+        }
+      },
+    },
+    // Question #3 - Project Title
     {
       type: "input",
       name: "title",
       message: "What is your project title? (Required)",
+      // Validation if nameInput is blank
       validate: (nameInput) => {
         if (nameInput) {
           return true;
@@ -24,11 +57,12 @@ inquirer
         }
       },
     },
-    // Question #2 - Project Description
+    // Question #4 - Project Description
     {
       type: "input",
       name: "description",
-      message: "What is your project's description? (Required)",
+      message: "Please write a short description of your project (Required)",
+      // Validation if nameInput is blank
       validate: (nameInput) => {
         if (nameInput) {
           return true;
@@ -38,45 +72,98 @@ inquirer
         }
       },
     },
-    // Question #3 - Installation Instructions
+    // Question #5 - License
     {
-      type: "input",
-      name: "install",
-      message: "Provide installation instructions for your project. (Required)",
+      type: "list",
+      name: "license",
+      message: "Select a license for your project. (Required)",
+      choices: [
+        "MIT",
+        "ISC",
+        "Microsoft Public License",
+        "The Unlicense",
+        "N/A",
+      ],
+      // Validation if nameInput is blank
       validate: (nameInput) => {
         if (nameInput) {
           return true;
         } else {
-          console.log("You need to provide installation instructions");
+          console.log("Please select a license!");
           return false;
         }
       },
     },
-    // Question #4 - Usage Info
+    // Question #5 - Installation Instructions
+    {
+      type: "list",
+      name: "install",
+      message: "What command should be run to install dependencies? (Required)",
+      choices: ["npm i", "npm install", "N/A"],
+      // Validation if nameInput is blank
+      validate: (nameInput) => {
+        if (nameInput) {
+          return true;
+        } else {
+          console.log("You need to select a command to install dependencies!");
+          return false;
+        }
+      },
+    },
+
+    // Question #6 - Tests
+    {
+      type: "list",
+      name: "tests",
+      message: "What command should be run to run tests? (Required)",
+      choices: ["npm test", "N/A"],
+      // Validation if nameInput is blank
+      validate: (nameInput) => {
+        if (nameInput) {
+          return true;
+        } else {
+          console.log("You need to select a command to run tests!");
+        }
+      },
+    },
+    // Question #6 - Usage Info
     {
       type: "input",
       name: "usage",
-      message: "Provide instructions and examples for use",
+      message:
+        "What does the user need to know about using this repo? (Optional)",
     },
-    // Question #5 - Contributing
+    // Question #7 - Contributing
     {
       type: "input",
-      name: "contributing",
+      name: "contributions",
       message:
-        "Provide any additional links for other users to contribute to. (Optional)",
+        "What does the user need to know about contributing to this repo? (Optional)",
     },
-    // Question #6 - Tests
-    {
-      type: "input",
-      name: "tests",
-      message:
-        "Go the extra mile and write tests for your application. Then provide examples on how to run them here. (Optional)",
-    },
-  ])
+  ]);
+};
+// TODO: Create a function to write responses into userResponses.json
+promptUser()
+  // .then((portfolioData) => {
+  //   return generatePage(portfolioData);
+  // })
+  // .then((markdownFile) => {
+  //   return writeFile(markdownFile);
+  // })
+  // .then((writeFileResponse) => {
+  //   console.log(writeFileResponse);
+  //   return copyFile();
+  // })
+  // .then((copyFileResponse) => {
+  //   console.log(copyFileResponse);
+  // })
+  // .catch((err) => {
+  //   console.log(err);
+  // });
 
-  // TODO: Create a function to write README file
   .then((data) => {
-    const fileName = `${data.name}.md`;
+    // Setting up file name
+    const fileName = `${data.fileName}.json`;
 
     fs.writeFile(fileName, JSON.stringify(data, null, "\t"), (err) =>
       err ? console.log(err) : console.log("You DID IT!")
